c2008afaa5ff20fc12f5bc1ba1873a5b2b63d17a
nopparut14546-collab <nopparut.14546@gmail.com>
2026-09-22 10:43:08 +0700

    Prob1_Checkpoint

--- Assets/Midterm Exam/Prob01-ClassDiagram/GameEntity.cs
+++ Assets/Midterm Exam/Prob01-ClassDiagram/GameEntity.cs
@@ -5,5 +5,28 @@ namespace MidtermExam.Prob01
     public class GameEntity
     {
         // TODO: Implement fields and methods according to Class Diagram
+        public string studentID = "1670703592";
+        protected int health;
+
+
+        public virtual void update()
+        {
+
+        }
+
+        public virtual void TakeDamage(int damage)
+        {
+
+        }
+
+        //void start()
+        //{
+
+        //}
+
+        //void update()
+        //{
+
+        //}
     }
 }
